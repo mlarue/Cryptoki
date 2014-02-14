000589dcf6eaf11830936e51558e4c22326c82b7
Markus Lauer <mlarue@usermode.de>
2014-02-14 20:02:51 +0100

    updated copyright text

--- lib/Crypt/Cryptoki.pm
+++ lib/Crypt/Cryptoki.pm
@@ -301,7 +301,7 @@ Markus Lauer, E<lt>mlarue@cpan.orgE<gt>
 Copyright (C) 2014 by Markus Lauer
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.18.2 or,
+it under the same terms as Perl itself, either Perl version 5.12.5 or,
 at your option, any later version of Perl 5 you may have available.
 
 

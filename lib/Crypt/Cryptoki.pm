12d538b9c554f6125b96d9646ca913aa58e8cc87
Markus Lauer <mlarue@usermode.de>
2014-09-16 12:31:05 +0200

    new version tag

--- lib/Crypt/Cryptoki.pm
+++ lib/Crypt/Cryptoki.pm
@@ -1,53 +1,7 @@
 package Crypt::Cryptoki;
-
-use 5.012005;
 use strict;
-use warnings;
 
-our $VERSION = '0.01_07';
-
-use Carp;
-use Moo;
-
-use Crypt::Cryptoki::Raw qw(rv_to_str CKR_OK);
-use Crypt::Cryptoki::Slot;
-use Crypt::Cryptoki::SlotWithToken;
-
-has 'module' => ( is => 'ro', required => 1 );
-has '_fl' => ( is => 'lazy' );
-
-sub _build__fl {
-	my $self = shift;
-	my $fl = Crypt::Cryptoki::Raw::load($self->module);
-	my $rv = $fl->C_Initialize;
-	if ( $rv != CKR_OK ) {
-		croak rv_to_str($rv);
-	}
-	return $fl;
-}
-
-sub info {
-	my $self = shift;
-	my $info = {};
-	my $rv = $self->_fl->C_GetInfo($info);
-	if ( $rv != CKR_OK ) {
-		croak rv_to_str($rv);
-	}
-	return $info;
-}
-
-sub slots {
-	my ( $self, %args ) = @_;
-	$args{token} ||= 0;
-	my $slots = [];
-	my $rv = $self->_fl->C_GetSlotList($args{token},$slots);
-	if ( $rv != CKR_OK ) {
-		croak rv_to_str($rv);
-	}
-	return $args{token} ?
-		map { Crypt::Cryptoki::SlotWithToken->new(ctx=>$self,id=>$_) } @$slots :
-		map { Crypt::Cryptoki::Slot->new(ctx=>$self,id=>$_) } @$slots;
-}
+our $VERSION = '0.01_09';
 
 1;
 __END__
@@ -57,47 +11,19 @@ Crypt::Cryptoki - Perl extension for PKCS#11
 
 =head1 SYNOPSIS
 
-	use Crypt::Cryptoki;
-
-	my $c = Crypt::Cryptoki->new('/usr/lib64/softhsm/libsofthsm.so');
-
-	my $info = $c->info;
-
-	for my $slot ( @{ $c->slots(1) } ) {
-		$slot->info;
-		$slot->token_info;
-	}
-
-	my $session = $c->open_session(
-		slot => $slot,
-		serial => 1,
-		rw => 1
-	);
-
-	$session->login_user('1234');
-
-	# or
-	$session->login_so('1234');
-
+	(in the meantime look at Crypt::Cryptoki::Raw)
 
 =head1 DESCRIPTION
 
-This module uses "Crypt::Cryptoki::Raw" to provide a object-oriented access to PKCS#11 instances.
+TBD: This module uses "Crypt::Cryptoki::Raw" to provide a object-oriented access to PKCS#11 instances.
 
 "RSA Security Inc. Public-Key Cryptography Standards (PKCS)"
 Please refer to Crypt::Cryptoki::Raw for more information about PKCS#11.
 
 
-
 =head2 FUNCTIONS
 
-	new
-
 =head2 METHODS
-
-	info
-	slots
-	open_session
 
 =head2 EXPORT
 
@@ -106,6 +32,8 @@ None by default.
 =head2 TODO
 
 =head1 SEE ALSO
+
+L<Crypt::Cryptoki::Raw>
 
 L<http://www.emc.com/emc-plus/rsa-labs/standards-initiatives/pkcs-11-cryptographic-token-interface-standard.htm>
 
